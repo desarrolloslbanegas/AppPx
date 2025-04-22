0ebb915062a5757f88f3758a3e2ffd0400cf59a1
Lucas Banegas <131389097+lmbanegas@users.noreply.github.com>
2025-04-22 15:26:57 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,17 @@ const loginValidation = require('../middlewares/loginValidation');
 router.get('/login', loginValidation, controller.login);
 router.post('/login',loginValidation, controller.loginPost);
 
+router.get('/ping-db', async (req, res) => {
+  try {
+    console.log("Conexión DB");
+    await pool.query('SELECT 1');
+    res.send('DB pinged successfully');
+  } catch (err) {
+    res.status(500).send('DB error: ' + err.message);
+  }
+});
+
+
 router.use(userValidation);
 
 router.get('/', controller.nuevaEntrada);
