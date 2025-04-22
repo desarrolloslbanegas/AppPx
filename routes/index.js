a2deb35da0e1d79d1f8953cb83b8613c66035a50
desarrolloslbanegas <desarrolloslbanegas@gmail.com>
2025-04-22 16:03:05 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.post('/login',loginValidation, controller.loginPost);
 
 router.get('/ping-db', async (req, res) => {
   try {
-    const query = 'INSERT INTO public."log" VALUES (4,23);
+    const query = 'INSERT INTO public."log" VALUES (4,23)';
     const result = await pool.query(query);
     
     console.log("Intento...")
